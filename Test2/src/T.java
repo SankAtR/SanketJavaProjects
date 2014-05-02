8eac3c531fb6ab3ad6a2b42cde1abcc65332bb04
SankAtR <sanketrao007@gmail.com>
2014-05-02 12:53:35 +0530

    First commit

---
+++ Test2/src/T.java
@@ -0,0 +1,24 @@
+import java.sql.Timestamp;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.PriorityQueue;
+
+
+public class T {
+
+	/**
+	 * @param args
+	 */
+	public static final SimpleDateFormat USER_FORMAT = new SimpleDateFormat("dd-mm-yyyy");	
+	public static void main(String[] args) throws ParseException {
+
+		Double d =23.3424234;
+		System.out.println(d);
+	
+	}	
+}
